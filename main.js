bfec9176e8a4677b9435b24b0d19e2b4daaf013a
Сончилеев Егор Валерьевич <74839766+SonchileevEgor@users.noreply.github.com>
2022-04-03 21:05:51 +0300

    Add files via upload
    Added clear button, drawing figure, axis

--- main.js
+++ main.js
@@ -38,3 +38,55 @@ function refresh()
 {
     document.location.reload();
 }
+
+function clearCanvas()
+{
+    var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    figure();
+}
+
+function polyLine(points)
+{
+    var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+    for (let i = 0; i < points.length; i++)
+    {
+        context.moveTo(points[i][0], points[i][1]);
+        console.log('moved to ' + points[i][0] + ', ' + points[i][1]);
+        context.lineTo(points[(i + 1) % points.length][0], points[(i + 1) % points.length][1]);
+        console.log('lined to ' + points[(i + 1) % points.length][0] + ', ' + points[(i + 1) % points.length][1]);
+        context.stroke();    
+    }
+}
+
+function figure()
+{
+    var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+
+    canvas.width = window.innerWidth / 2;
+    canvas.height = window.innerHeight;
+    context.fillStyle = '#008080';
+
+    context.moveTo(canvas.width / 2, 0);
+    context.lineTo(canvas.width / 2, canvas.height);
+    context.stroke();
+
+    context.moveTo(0, canvas.height / 2);
+    context.lineTo(canvas.width, canvas.height / 2);
+    context.stroke();
+
+    var figure_bot = [[0 + canvas.width / 2, 0 + canvas.width / 2], [-100 + canvas.width / 2, -100 + canvas.width / 2], [100 + canvas.width / 2, -100 + canvas.width / 2]];
+    polyLine(figure_bot);
+
+    var figure_mid = [[-100 + canvas.width / 2, -100 + canvas.width / 2], [100 + canvas.width / 2, -100 + canvas.width / 2], [100 + canvas.width / 2, 320], [-100 + canvas.width / 2, 320]];
+    polyLine(figure_mid);
+
+    var figure_top = [[-80 + canvas.width / 2, 320], [-100 + canvas.width / 2, 290], [-60 + canvas.width / 2, 290], [canvas.width / 2, 270], [60 + canvas.width / 2, 290], [100 + canvas.width / 2, 290], [80 + canvas.width / 2, 320]];
+    polyLine(figure_top);
+
+}
